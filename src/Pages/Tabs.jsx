58dca84e9ba079ac60e6ed87ba0e6b30f7aca32d
Muhammad Ramdhan Azizi <115683965+Azizi123haha@users.noreply.github.com>
2024-10-27 23:35:01 +0700

    Update Tabs.jsx

--- src/Pages/Tabs.jsx
+++ src/Pages/Tabs.jsx
@@ -9,7 +9,6 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import PIcon from "../Components/CardIcon";
 import { styled } from "@mui/system";
 
 function TabPanel(props) {
@@ -54,7 +53,7 @@ function a11yProps(index) {
 
 const StyledBox = styled(Box)({
   padding: "20px",
-  backgroundColor: "#333", // Warna latar belakang card yang gelap
+  backgroundColor: "#333333", // Warna latar belakang card yang gelap
   borderRadius: "10px", // Membuat sudut card melengkung
   boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)", // Memberikan bayangan
   color: "#ced4d7", // Warna teks yang kontras
@@ -71,24 +70,6 @@ export default function FullWidthTabs() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
-  const sections = [
-    {
-      title: "Designing",
-      description:
-        "I love designing. When I see something that needs improvement, I enjoy making it better visually. I pay attention to every detail, whether it’s a graphic or a user interface. My goal is to create designs that catch the eye and inspire others.",
-    },
-    {
-      title: "Developing",
-      description:
-        "After designing, I dive into development. Coding is where I bring designs to life, turning concepts into digital experiences. Each line of code reflects my vision for functionality and elegance, showcasing my commitment to excellence.",
-    },
-    {
-      title: "Expanding",
-      description:
-        "Expanding beyond creation. I optimize templates to full websites. I refine post-development. Leveraging blogging and basic SEO, I enhance visibility and impact online. Each step maximizes reach and effectiveness online.",
-    },
-  ];
 
   const projects = [
     {
@@ -127,8 +108,6 @@ export default function FullWidthTabs() {
             }}
           >
             <Tab label="Projects" {...a11yProps(0)} sx={{ fontWeight: "bold", color: "#ced4d7", fontSize: ["1rem", "2rem"], marginBottom: "10px" }} />
-            <Tab label="Things I Love" {...a11yProps(1)} sx={{ fontWeight: "bold", color: "#ced4d7", fontSize: ["1rem", "2rem"], marginBottom: "10px" }} />
-            <Tab label="Skills" {...a11yProps(2)} sx={{ fontWeight: "bold", color: "#ced4d7", fontSize: ["1rem", "2rem"], marginBottom: "10px" }} />
           </Tabs>
         </AppBar>
         <SwipeableViews axis={theme.direction === "rtl" ? "x-reverse" : "x"} index={value} onChangeIndex={setValue}>
@@ -167,32 +146,6 @@ export default function FullWidthTabs() {
                   </Box>
                 </StyledBox>
               ))}
-            </div>
-          </TabPanel>
-          <TabPanel value={value} index={1} dir={theme.direction}>
-            <div className="container mx-auto flex flex-col items-center">
-              {sections.map((section, index) => (
-                <div key={index} className="p-5 text-center" data-aos="fade-left" data-aos-delay={`${index * 100}`}>
-                  <Typography variant="h6" sx={{ fontWeight: "bold", color: "#fff" }}>{section.title}</Typography>
-                  <Typography sx={{ color: "#ced4d7" }}>{section.description}</Typography>
-                </div>
-              ))}
-            </div>
-          </TabPanel>
-          <TabPanel value={value} index={2} dir={theme.direction}>
-            <div className="container mx-auto flex justify-center items-center overflow-hidden" data-aos="fade-up" data-aos-delay="200">
-              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
-                <PIcon PIcon="html.svg" Language="HTML" />
-                <PIcon PIcon="css.svg" Language="CSS" />
-                <PIcon PIcon="javascript.svg" Language="JavaScript" />
-                <PIcon PIcon="tailwind.svg" Language="Tailwind CSS" />
-                <PIcon PIcon="reactjs.svg" Language="ReactJS" />
-                <PIcon PIcon="vite.svg" Language="Vite" />
-                <PIcon PIcon="nodejs.svg" Language="Node JS" />
-                <PIcon PIcon="bootstrap.svg" Language="Bootstrap" />
-                <PIcon PIcon="firebase.svg" Language="Firebase" />
-                <PIcon PIcon="MUI.svg" Language="Material UI" />
-              </div>
             </div>
           </TabPanel>
         </SwipeableViews>
